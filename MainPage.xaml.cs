14589bf70944f036d7d9ca69598b0a1e9c8207bc
Vasily Fomin <vasili.fomin@gmail.com>
2011-07-22 20:57:09 +0200

    Added Quote class. Added demo request sender.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Windows.Browser;
 
 namespace ForismaticGadget
 {
@@ -17,6 +18,33 @@ namespace ForismaticGadget
         public MainPage()
         {
             InitializeComponent();
+            quoteText = new TextBlock();
+        }
+
+        private void LogoClicked(object sender, MouseButtonEventArgs e)
+        {
+            HtmlPage.Window.Navigate(new Uri("http://forismatic.com", UriKind.Absolute), "_blank");
+        }
+
+        private void RefreshClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show( Quote.Parse("<forismatic><quote><quoteText>Лучшее украшение жизни — хорошее настроение. </quoteText><quoteAuthor>Алексей Батиевский</quoteAuthor><senderName></senderName><senderLink></senderLink><quoteLink>http://ru.forismatic.com/a1865c23cd/</quoteLink></quote></forismatic>").ToString() );
+            //WebClient webClient = new WebClient();
+            //string apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&format=xml&lang=ru";
+
+            //webClient.DownloadStringCompleted +=new DownloadStringCompletedEventHandler(ReadAnswer);
+            //webClient.DownloadStringAsync(new Uri(apiUrl));
+        }
+
+        private static void ReadAnswer(Object sender, DownloadStringCompletedEventArgs e)
+        {
+            if ( !e.Cancelled && e.Error == null )
+            {
+                string answer = e.Result.ToString();
+                MessageBox.Show(answer);
+
+                //quoteText.Text = answer;
+            }
         }
     }
 }
---
+++ Quote.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.IO;
+
+namespace ForismaticGadget
+{
+	public class Quote 
+	{	
+		public String Text;
+	
+		public String Author;
+
+		public String Link;
+	
+		public Quote () { }
+	
+		public Quote(String text, String author) 
+		{
+			Text = text;
+			Author = author;
+		}
+	
+		public static Quote Parse(String forsmaticResponseString)
+		{
+			Quote quote = new Quote();
+
+			// Create an XmlReader
+			using (XmlReader reader = XmlReader.Create(new StringReader(forsmaticResponseString)))
+			{
+                while ( reader.Read() )
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "quoteText":
+                                reader.Read();
+                                quote.Text = reader.Value;
+                                break;
+                            
+                            case "quoteAuthor":
+                                reader.Read();
+                                quote.Author = reader.Value;
+                                break;
+
+                            case "quoteLink":
+                                reader.Read();
+                                quote.Link = reader.Value;
+                                break;                            
+                        }
+                    }
+                }               
+			}
+		
+			return quote;
+		}
+
+		public override string ToString()
+		{
+			return String.Format("Text:{0}\nAuthor:{1}\nLink:{2}\n", Text, Author, Link);
+		}
+	}
+}
